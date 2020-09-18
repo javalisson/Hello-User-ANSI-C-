3846afa6e7e8be0254b0d3d65641b1558fc2fa84
Alisson Prestes <javalisson@gmail.com>
2020-09-18 16:31:38 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+#include <limits.h>
+
+int main(void) {
+  char name[LINE_MAX] = "World\n";
+  char input[LINE_MAX];
+
+  printf("Type your name:\n");
+  fgets(input, LINE_MAX, stdin);
+
+  // this removes the new line from the end of the string
+  input[strcspn(input, "\n")] = 0;
+
+  if (strcmp(input, "\n") != 0) {
+    strcpy(name, input);
+  }
+
+  printf("Hello, %s!\n", name);
+
+  return 0;
+}
